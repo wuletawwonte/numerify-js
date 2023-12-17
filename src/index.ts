735c88f506bd80e693c07c7523fd3cc3786532f8
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-17 19:37:08 +0300

    Add support for string inputs

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,17 @@ const convertFunctions: ConvertFunctions = {
   geez: geez.convert
 }
 
-const convert = (number: number, to: string) => {
+const convert = (number: number | string, to: string) => {
   const convertFunction = convertFunctions[to]
+  const inputNumber = parseFloat(number as string)
 
   if (convertFunction) {
-    return convertFunction(number)
+    return convertFunction(inputNumber)
   } else {
     throw new Error(`No converter for ${to}`)
   }
 }
 
-console.log(convert(1, 'geez'))
+console.log(convert("1", 'geez'))
 
 export default convert;
