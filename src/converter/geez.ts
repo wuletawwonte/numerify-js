a58c37f091eacc54b61ec0cd492d8865ab07e544
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-18 16:43:14 +0300

    Format all files using prettier

--- src/converter/geez.ts
+++ src/converter/geez.ts
@@ -1,5 +1,5 @@
 export default {
   convert: () => {
-      return "geez";
-  }
-}
+    return "geez";
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import geez from './converter/geez.js';
+import geez from "./converter/geez.js";
 
 interface ConvertFunctions {
   [key: string]: (number: number) => string;
 }
 
 const convertFunctions: ConvertFunctions = {
-  geez: geez.convert
-}
+  geez: geez.convert,
+};
 
 const convert = (inputNumber: number | string, to: string) => {
-  const convertFunction = convertFunctions[to]
+  const convertFunction = convertFunctions[to];
 
   if (convertFunction) {
-    return convertFunction(+inputNumber)
+    return convertFunction(+inputNumber);
   } else {
-    throw new Error(`No converter for ${to}`)
+    throw new Error(`No converter for ${to}`);
   }
-}
+};
 
-console.log(convert("1", 'geez'))
+// console.log(convert("1", "geez"));
 
 export default convert;
