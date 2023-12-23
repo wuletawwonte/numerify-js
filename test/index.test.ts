1cb8a4388f9fc7ac9b8aa7f1359b5000c240da92
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-23 20:49:50 +0300

    Add one unit test function

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,3 +3,7 @@ import convert from '../src/index'
 it('should convert 1 to geez', () => {
   expect(convert(1)).toBe('፩')
 })
+
+it('should convert 12 to geez', () => {
+  expect(convert(12)).toBe('፲፪')
+})
