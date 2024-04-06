a0fb1e091bd014a6f2e49b22811f256777e58cb4
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-04-06 19:17:10 +0300

    move the default export statment to the declaration statement

--- src/factory.ts
+++ src/factory.ts
@@ -1,11 +1,12 @@
 import Geez from './converter/geez.js'
 
-const NumeralFactory = (inputNumber: number | string, className: string) => {
+export default function NumeralFactory(
+  inputNumber: number | string,
+  className: string
+) {
   if (className === 'geez') {
     return new Geez(inputNumber)
   } else {
     return null
   }
 }
-
-export default NumeralFactory
